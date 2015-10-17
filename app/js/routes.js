c76c9faf776395128ff90bf23a520868d8a67806
peter <peter.k.nets@gmail.com>
2015-10-17 18:26:48 +0300

    Add more routes

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,7 +3,18 @@
  */
 angular.module('myApp')
   .config(function($routeProvider) {
-    $routeProvider.when('/about', {
-      templateUrl: 'templates/pages/about/index.html'
-    })
+    $routeProvider
+      .when('/about', {
+        templateUrl: 'templates/pages/about/index.html'
+      })
+      .when('/notes', {
+        templateUrl: 'templates/pages/notes/index.html'
+      })
+      .when('/users', {
+        templateUrl: 'templates/pages/users/index.html'
+      })
+      .when('/', {
+        templateUrl: 'templates/pages/about/index.html'
+      })
+      .otherwise({ redirectTo: '/'});
   });
